bc810e82c33743f6652d2e468010c59f2616137d
Khodairy <Abdallakhodairy14@gmail.com>
2026-02-22 16:52:17 +0200

    updating validations

--- src/common/middleware/validations.js
+++ src/common/middleware/validations.js
@@ -1,12 +1,19 @@
 export const Validation = (schema) => {
   return (req, res, next) => {
-    const { error } = schema.validate(req.body, {
-      abortEarly: false,
-    });
+    const errorResult = [];
+    for (let key of Object.keys(schema)) {
+      const { error } = schema[key].validate(req[key], {
+        abortEarly: false,
+      });
 
-    if (error) {
-      console.log(error);
-      throw new Error(error.message);
+      if (error) {
+        console.log(error);
+        errorResult.push(error.message);
+        // throw new Error(error.message);
+      }
+    }
+    if (errorResult.length > 0) {
+      res.status(400).json({ message: errorResult });
     }
     next();
   };
--- src/module/users/user.schema.js
+++ src/module/users/user.schema.js
@@ -5,44 +5,50 @@ import {
   roleEnum,
 } from "../../common/enum/user.enum.js";
 
-export const signUp_Schema = Joi.object({
-  userName: Joi.string().min(3).max(30).required(),
+export const signUp_Schema = {
+  body: Joi.object({
+    userName: Joi.string().min(3).max(30).required(),
 
-  email: Joi.string()
-    .email({ tlds: { allow: ["com", "net", "org"] } })
-    .required(),
+    email: Joi.string()
+      .email({ tlds: { allow: ["com", "net", "org"] } })
+      .required(),
 
-  password: Joi.string().min(6).max(20).required(),
+    password: Joi.string().min(6).max(20).required(),
 
-  phone: Joi.string().pattern(/^01[0125][0-9]{8}$/),
+    phone: Joi.string().pattern(/^01[0125][0-9]{8}$/),
 
-  age: Joi.number().integer().min(15).max(80),
+    age: Joi.number().integer().min(15).max(80),
 
-  gender: Joi.string()
-    .valid(genderEnum.male, genderEnum.female)
-    .default(genderEnum.male),
+    gender: Joi.string()
+      .valid(genderEnum.male, genderEnum.female)
+      .default(genderEnum.male),
 
-  provider: Joi.string()
-    .valid(providerEnum.system, providerEnum.google)
-    .default(providerEnum.system),
+    provider: Joi.string()
+      .valid(providerEnum.system, providerEnum.google)
+      .default(providerEnum.system),
 
-  role: Joi.string()
-    .valid(roleEnum.user, roleEnum.admin)
-    .default(roleEnum.user),
+    role: Joi.string()
+      .valid(roleEnum.user, roleEnum.admin)
+      .default(roleEnum.user),
 
-  profilePic: Joi.string().allow(""),
+    profilePic: Joi.string().allow(""),
 
-  confirmed: Joi.boolean().default(false),
-});
+    confirmed: Joi.boolean().default(false),
+  }),
+};
 
-export const signUpWithGmail_Schema = Joi.object({
-  idToken: Joi.string().required(),
-});
+export const signUpWithGmail_Schema = {
+  body: Joi.object({
+    idToken: Joi.string().required(),
+  }),
+};
 
-export const signIn_Schema = Joi.object({
-  email: Joi.string()
-    .email({ tlds: { allow: ["com", "net", "org"] } })
-    .required(),
+export const signIn_Schema = {
+  body: Joi.object({
+    email: Joi.string()
+      .email({ tlds: { allow: ["com", "net", "org"] } })
+      .required(),
 
-  password: Joi.string().min(6).max(20).required(),
-});
+    password: Joi.string().min(6).max(20).required(),
+  }),
+};
